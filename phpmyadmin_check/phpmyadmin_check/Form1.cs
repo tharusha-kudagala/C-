fea29c2abfccfcd5acbe7609a6e7ca4141c220f2
Tharusha Kudagala <44969527+tharusha-kudagala@users.noreply.github.com>
2019-07-17 11:34:52 +0530

    Add files via upload

---
+++ phpmyadmin_check/phpmyadmin_check/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+
+namespace phpmyadmin_check
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains("D") && textBox1.Text.Length == 4)
+            {
+                drlog obj = new drlog();
+                this.Hide();
+                obj.Show();
+            }
+            else if (textBox1.Text.Contains("P") && textBox1.Text.Length == 3)
+            {
+                pharlog obj = new pharlog();
+                this.Hide();
+                obj.Show();
+            }
+            else if (textBox1.Text.Contains("A") && textBox1.Text.Length == 2)
+            {
+                Adminlog obj = new Adminlog();
+                this.Hide();
+                obj.Show();
+            }
+            else
+                MessageBox.Show("Invalid Username");
+
+        }
+    }
+}
